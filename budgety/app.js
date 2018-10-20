1ffe461134e549c7a67d7836ca1c5c340fe4f84b
Kristie Nguyen <kristietamato@gmail.com>
2018-10-20 13:52:13 -0700

    Add initial modules

--- budgety/app.js
+++ budgety/app.js
@@ -1,1 +1,40 @@
 // Budgety App
+
+// UI Module tasks:
+// Get input values
+// Add the new item to the UI
+// Update the UI
+
+// Data Module tasks:
+// Add the new item to our data structure
+// Calculate budget
+
+// Controller module tasks:
+// Add event handler
+
+var budgetController = (function() {
+  var x = 23;
+  var add = function(a) {
+    return x + a;
+  }
+
+  return {
+    publicTest: function(b) {
+      return add(b);
+    }
+  }
+})();
+
+var UIController = (function() {
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+  var z = budgetCtrl.publicTest(5);
+
+  return {
+    anotherPublic: function() {
+      console.log(z);
+    }
+  }
+})(budgetController, UIController);
